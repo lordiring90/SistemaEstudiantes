669dd3481cab64df8d45992d2949fb032ff31a7a
lordiring90 <lordiring2000@gmail.com>
2026-02-20 17:47:00 -0500

    ahora si

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,4 +1,89 @@
-package PACKAGE_NAME;
-
 public class Estudiante {
+    private String nombre;
+    private double nota1;
+    private double nota2;
+    private double nota3;
+
+    public Estudiante() {
+    }
+//construcctor con todos los parametros
+    public Estudiante(String nombre, double nota1, double nota2, double nota3) {
+        this.nombre = nombre;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+    }
+
+    public Estudiante(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public double getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(double nota3) {
+        this.nota3 = nota3;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante{" +
+                "nombre='" + nombre + '\'' +
+                ", nota1=" + nota1 +
+                ", nota2=" + nota2 +
+                ", nota3=" + nota3 +
+                '}';
+    }
+
+    //metodos propios
+    public void mostrarInformacion() {
+        System.out.println("Nombre: " + this.getNombre());
+        System.out.println("nota1: " + this.getNota1());
+        System.out.println("nota2: " + this.getNota2());
+        System.out.println("nota3: " + this.getNota3());
+    }
+    public void asignarNotas(double n1, double n2, double n3){
+        this.nota1 = n1;
+        this.nota2 = n2;
+        this.nota3 = n3;
+    }
+
+
+    public double calcularPromedio(){
+        return (this.nota1+this.nota2+this.nota3)/3;
+    }
+
+    public String estaAprobado(double notaMinima){
+        double promedio = calcularPromedio();
+        if(promedio>=notaMinima){
+            return ("Esta aprobado");
+        } else {
+            return ("Esta desaprobado");
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) { // <--- Te faltaba esta línea mágica
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // 1. Creamos el objeto Sofia
+        Estudiante e1 = new Estudiante("Sofia", 3.5, 3.9, 4.5);
+        // 2. Usamos tus métodos propios
+        System.out.println("========================================");
+        e1.mostrarInformacion();
+
+        System.out.println("El promedio es: " + e1.calcularPromedio());
+        System.out.println("Estado: " + e1.estaAprobado(3.0));
+        System.out.println();
+        System.out.println("========================================");
+        Estudiante e2 = new Estudiante("laura");
+        e2.asignarNotas(2.1,3.1,1.9);
+        e2.mostrarInformacion();
+        System.out.println("El promedio es: " + e2.calcularPromedio());
+        System.out.println("Estado: " + e2.estaAprobado(4.0));
+        System.out.println("========================================");
     }
 }
